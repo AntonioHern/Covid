058fe90024a4cd66401136487231cf3b48948aa4
AntonioHern <antoniohern@gmail.com>
2020-05-26 18:47:12 +0200

    fechas

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,8 @@ Route::get('/', function () {
     return view('dashboard');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
 
-Route::group(['middleware' => 'auth'], function () {
-	Route::resource('user', 'UserController', ['except' => ['show']]);
-	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
-	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
-});
+
 
