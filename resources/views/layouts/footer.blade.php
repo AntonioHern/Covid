9f93b2ca78ef77c51c2cc68405d895d5fd07a9e8
AntonioHern <antoniohern@gmail.com>
2020-05-27 11:41:54 +0200

    CAMBIO DE API

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
     <div class="row justify-content-center">
         <div class="copyright mr-4">
-            &copy; {{ now()->year }} {{ __('made with') }} <i class="tim-icons icon-heart-2"></i> {{ __('by') }}
-            <a href="https://creative-tim.com" target="_blank">{{ __('AntonioHern') }}</a> &amp;
-            <a href="https://updivision.com" target="_blank"></a> {{ __('for a better web') }}.
+            &copy; {{ now()->year }} {{ __('Fuentes de datos:') }}
+            <a href="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/situacionActual.htm" target="_blank">{{ ' mscbs.gob.es y Depts. de Salud de las CCAA' }}</a>
+
         </div>
     </div>
 </footer>
