ec281a428dbeb30d7b4120fdc480025a5098032f
AntonioHern <antoniohern@gmail.com>
2020-05-28 09:05:21 +0200

    error con recuperados

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center">
         <div class="copyright mr-4">
             &copy; {{ now()->year }} {{ __('Fuentes de datos:') }}
-            <a href="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/situacionActual.htm" target="_blank">{{ ' mscbs.gob.es y Depts. de Salud de las CCAA' }}</a>
+            <a href="https://coronavirus.secuoyas.com/" target="_blank">{{ ' coronavirus.secuoyas.com' }}</a>
 
         </div>
     </div>
