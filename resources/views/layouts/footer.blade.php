d39a8379483c108a4f31b73af5f2cda755bfcfb9
AntonioHern <antoniohern@gmail.com>
2020-08-28 11:40:36 +0200

    Autor

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="row justify-content-center">
         <div class="copyright mr-4">
-            &copy; {{ now()->year }} {{ __('Fuentes de datos:') }}
+        {{'AntonioHern'}}  &copy; {{ now()->year }}. {{ __('Fuentes de datos:') }}
             <a href="https://covid19tracking.narrativa.com/" target="_blank">{{ 'covid19tracking.narrativa.com ' }}</a>
 
         </div>
